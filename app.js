ef8639249d35d7f94789b0628095a7533eba1b2f
ZisZtR <zisztr@gmail.com>
2023-03-10 14:19:03 +0700

    finish dynamic deliverList

--- app.js
+++ app.js
@@ -51,16 +51,52 @@ async function addOrdering(){
 
 }
 
-{/* <div class="itemInfo">
-    <b>1</b>
-    <div class="deliverItem" id="item0">
-        <h2>Order ID : xxxx</h2>
-        <h2>Medicine List</h2>
-        <h3><pre>   Med 1 : 1/1</pre></h3>
-        <h3><pre>   Med 2 : 2/3</pre></h3>
-    </div>
-</div> */}
+// GET /order_get/{n} n start at 0
+// orderList{
+//     "1" : 2,
+//     "2" : 3,
+//     "3" : 4
+// }
+function addDeliverOrder(num, orderID, orderList){
+    // <div class="itemInfo">
+    //     <b>1</b>
+    //     <div class="deliverItem" id="item0">
+    //         <h2>Order ID : xxxx</h2>
+    //         <h2>Medicine List</h2>
+    //         <h3><pre>   Med 1 : 1/1</pre></h3>
+    //         <h3><pre>   Med 2 : 2/3</pre></h3>
+    //     </div>
+    // </div>
 
+    const tmp = document.createElement('div')
+    tmp.classList.add("itemInfo")
+
+    const numtmp = document.createElement('b')
+    numtmp.innerHTML = `${num+1}`
+    tmp.appendChild(numtmp)
+
+    const deliver = document.createElement('div')
+    deliver.classList.add("deliverItem")
+    deliver.id = `item${num}`
+
+    const txt1 = document.createElement('h2')
+    txt1.innerHTML = `Order ID : ${orderID}`
+    deliver.appendChild(txt1)
+
+    const txt2 = document.createElement('h2')
+    txt2.innerHTML = 'Medicine List'
+    deliver.appendChild(txt2)
+
+    for(let i=0;i<Object.keys(orderList).length;i++){
+        let txt3 = document.createElement('h3')
+        txt3.innerHTML = `<pre>   Med ${i+1} : 0/${orderList[`${i+1}`]}</pre>`
+        deliver.appendChild(txt3)
+    }
+
+    tmp.appendChild(deliver)
+
+    document.getElementById('deliverList').appendChild(tmp)
+}
 
 async function getData(){
     const response = await fetch('')
@@ -72,5 +108,12 @@ async function getData(){
     // const cabList = await getData()
 })
 
+
+const deliverTMP = [ { orderID : 'xxxx', list : { "1" : 1, "2" : 3 } }, { orderID : 'yyyy', list : { "1" : 0, "2" : 1 } }, { orderID : 'zzzz', list : { "1" : 5, "2" : 1 } }, { orderID : 'aaaa', list : { "1" : 1, "2" : 3 } } ]
+
 addItemOrder(1)
 addItemOrder(2)
+for(let i=0;i<deliverTMP.length;i++){
+    // console.log(deliverTMP[i])
+    addDeliverOrder(i, deliverTMP[i]['orderID'], deliverTMP[i]['list'])
+}
